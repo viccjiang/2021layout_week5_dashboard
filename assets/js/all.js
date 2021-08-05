6d7aaa5c71920a7013fcfab4de9336ed182b8a10
viccjiang <binbinbb520@gmail.com>
2021-08-05 18:24:33 +0800

    Update 2021-08-05T10:24:27.585Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,8 +1,2 @@
 "use strict";
-
-ClassicEditor.create(document.querySelector('#editor')).then(function (editor) {
-  console.log(editor);
-})["catch"](function (error) {
-  console.error(error);
-});
 //# sourceMappingURL=all.js.map
