e3f5ad47d95c535c6ea7a55015f66db2dba86008
viccjiang <binbinbb520@gmail.com>
2021-08-05 16:42:03 +0800

    Update 2021-08-05T08:41:57.998Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,8 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
+ClassicEditor.create(document.querySelector('#editor')).then(function (editor) {
+  console.log(editor);
+})["catch"](function (error) {
+  console.error(error);
 });
 //# sourceMappingURL=all.js.map
