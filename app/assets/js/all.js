b53890ef102d2975420c8b54130b363e26f48fb8
viccjiang <binbinbb520@gmail.com>
2021-08-05 16:39:45 +0800

    add admin

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,12 @@
-$(function() {
-  console.log('Hello Bootstrap5');
-});
+
+
+ClassicEditor
+  .create(document.querySelector('#editor'))
+  .then(editor => {
+    console.log(editor);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+
