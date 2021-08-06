acea753a4787a65cd5f30603caf570b23ff56d7f
viccjiang <binbinbb520@gmail.com>
2021-08-06 17:37:08 +0800

    Update 2021-08-06T09:37:03.384Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,2 +1,15 @@
 "use strict";
+
+ClassicEditor.create(document.querySelector('#editor'))["catch"](function (error) {
+  console.error(error);
+});
+$(".transactionType").on("click", function () {
+  $(".fadeInUpBig").show();
+  $("#editor").stop(true, true).animate({
+    height: "300px"
+  }, 500);
+});
+$(".site_item1").on("click", function () {
+  $(".fadeInUpBig").hide();
+});
 //# sourceMappingURL=all.js.map
